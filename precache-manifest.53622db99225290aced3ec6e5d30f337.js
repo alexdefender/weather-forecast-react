c13cdd8603bbb380188c9796ba9b7e304d546758
Alex <alexiv1102@gmail.com>
2019-04-24 11:33:30 +0300

    Updates

---
+++ precache-manifest.53622db99225290aced3ec6e5d30f337.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "445ca56c383cc9bd1c8e04db24e42746",
+    "url": "/weather-forecast-react/index.html"
+  },
+  {
+    "revision": "c9ded4dd3c4d8858145a",
+    "url": "/weather-forecast-react/static/css/main.ce73b255.chunk.css"
+  },
+  {
+    "revision": "3adf846ef00e22461c39",
+    "url": "/weather-forecast-react/static/js/2.787b0fcc.chunk.js"
+  },
+  {
+    "revision": "c9ded4dd3c4d8858145a",
+    "url": "/weather-forecast-react/static/js/main.338c8ba4.chunk.js"
+  },
+  {
+    "revision": "999f58880192b403a780",
+    "url": "/weather-forecast-react/static/js/runtime~main.9b53f0c8.js"
+  }
+]);
