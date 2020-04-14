b3998fb4a575c112081d517cdbceb93910138386
Aleksey Ivanov <alex.iv1102@gmail.com>
2020-04-14 11:31:15 +0300

    Updates

---
+++ precache-manifest.0ad3b40fd0c07152fe10e575bf58ab3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02218e4e4c422e7ba977af8d95ed59a5",
+    "url": "/weather-forecast-react/index.html"
+  },
+  {
+    "revision": "b7621366f1c0ffe46a13",
+    "url": "/weather-forecast-react/static/css/main.b80ad192.chunk.css"
+  },
+  {
+    "revision": "e7693729bcc8a270026c",
+    "url": "/weather-forecast-react/static/js/2.a94dcd6b.chunk.js"
+  },
+  {
+    "revision": "b7621366f1c0ffe46a13",
+    "url": "/weather-forecast-react/static/js/main.dcc38db0.chunk.js"
+  },
+  {
+    "revision": "999f58880192b403a780",
+    "url": "/weather-forecast-react/static/js/runtime~main.9b53f0c8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/weather-forecast-react/precache-manifest.03434843dfc032d6a9c3597d443c2775.js"
+  "/weather-forecast-react/precache-manifest.0ad3b40fd0c07152fe10e575bf58ab3b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/weather-forecast-react/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
